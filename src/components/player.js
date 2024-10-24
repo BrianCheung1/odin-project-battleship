ada7bceacc2dfea38d749f0a2dc5384f732e20f8
Brian Cheung <44447718+BrianCheung1@users.noreply.github.com>
2024-10-24 15:12:16 -0400

    added opponent board paramter to makeMove

--- src/components/player.js
+++ src/components/player.js
@@ -7,16 +7,13 @@ class Player {
     this.board = new Gameboard()
   }
 
-  makeMove(x, y) {
+  makeMove(x, y, opponentBoard) {
     if (this.type === "Player") {
-      return this.board.receiveAttack(x, y)
+      return opponentBoard.receiveAttack(x, y) 
     } else if (this.type === "Computer") {
-      const randomX = Math.floor(Math.random() * this.board.size)
-      const randomY = Math.floor(Math.random() * this.board.size)
-
-      return this.board.receiveAttack(randomX, randomY)
+      return opponentBoard.receiveAttack(x, y) 
     }
   }
 }
 
-export {Player}
+export { Player }
